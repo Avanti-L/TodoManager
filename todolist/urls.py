c823795646c3898ebe61f281ee437e4a766c32e3
Avanti-L <avantilabde8@gmail.com>
2025-12-28 19:19:06 +0530

    Add templates

--- todolist/urls.py
+++ todolist/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+    path('todolist/', views.todolist, name="todolist"),
+    path('contact/', views.contact, name="contact"),
+    path('about/', views.about, name="about"),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -3,4 +3,25 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, "base.html", {}) 
+    context = {
+        'page': 'Todo Manager'
+    }
+    return render(request, "home.html", context) 
+
+def todolist(request):
+    context = {
+        'page': 'Todo List'
+    }
+    return render(request, "todolist.html", context) 
+
+def contact(request):
+    context = {
+        'page': 'Contact Us'
+    }
+    return render(request, "contact.html", context) 
+
+def about(request):
+    context = {
+        'page': 'About Us'
+    }
+    return render(request, "about.html", context) 
