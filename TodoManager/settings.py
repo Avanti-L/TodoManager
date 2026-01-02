ed9acc979228086d23729956fd810bb5dc3c15d9
Avanti-L <avantilabde8@gmail.com>
2026-01-02 17:59:41 +0530

    Configure MySQL database

--- TodoManager/settings.py
+++ TodoManager/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'TodoManager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo_db',
+        'USER': 'root',
+        'PASSWORD': 'L@,la.lablah33',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
