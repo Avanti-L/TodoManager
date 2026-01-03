242cd8a3f205a7c77b47fd32c091adfa961040f8
Avanti-L <avantilabde8@gmail.com>
2026-01-03 09:49:56 +0530

    Add a todo

--- todolist/views.py
+++ todolist/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from todolist.models import Task
+from todolist.forms import TaskForm
 
 # Create your views here.
 def home(request):
@@ -10,6 +11,12 @@ def home(request):
     return render(request, "home.html", context) 
 
 def todolist(request):
+    if request.method == "POST":
+        form_data = TaskForm(request.POST or None)
+        if form_data.is_valid():
+            form_data.save()
+            return redirect("todolist")
+        
     all_tasks = Task.objects.all()
     context = {
         'page': 'Todo List',
